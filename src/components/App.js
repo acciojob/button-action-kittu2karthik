edbad5179b0c6bcb8ea8d28ace05c91fa0aa3cf5
kittu2karthik <kittu2karthik@gmail.com>
2025-11-28 14:01:46 +0530

    completed the requirements

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,31 @@
-import React, { useState } from "react";
-import './../styles/App.css';
+import React, { useEffect, useState } from "react";
+import "./../styles/App.css";
 
 const App = (props) => {
+  const paraRef = useRef(null);
+
+  function handleClick() {
+    if (paraRef.current) {
+      const paragraph = paraRef.current;
+      if (paragraph.classList.contains("hide")) {
+        paragraph.classList.remove("hide");
+        paragraph.classList.add("show");
+      } else {
+        paragraph.classList.remove("show");
+        paragraph.classList.add("hide");
+      }
+    }
+  }
 
   return (
     <div className="App" id="main">
-      // Do not alter the main div
+      <p id="para" className="hide" ref={paraRef}>
+        Hello, I've learnt to use the full-stack evaluation tool. This makes me
+        so happy
+      </p>
+      <button id="click" onClick={handleClick}></button>
     </div>
   );
-}
+};
 
-export default App
+export default App;
